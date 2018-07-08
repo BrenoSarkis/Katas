a24e833ed818591c5232c1105baf90d3524a5a1c
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-08 21:01:29 +0200

    [PrimeFactors] - Prime factors of 1 - Transforming from null to constant

--- Katas.PrimeFactors/PrimeFactorsTests.cs
+++ Katas.PrimeFactors/PrimeFactorsTests.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Katas.PrimeFactors
 {
-    class PrimeFactorsTests
+    [TestFixture]
+    public class PrimeFactorsTests
     {
+        [Test]
+        public void Factors()
+        {
+            var primeFactors = PrimeFactorsCalculator.PrimeFactorsOf(1);
+            Assert.That(primeFactors, Is.Empty);
+        }
+    }
+
+    public class PrimeFactorsCalculator
+    {
+        public static List<int> PrimeFactorsOf(int n)
+        {
+            return new List<int>();
+        }
     }
 }
