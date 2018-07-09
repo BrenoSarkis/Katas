56f42ace35e063bd88cfec076e044345c410f83d
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-09 19:51:41 +0200

    [FizzBuzz] - One returns One - Transformation: Null to constant

--- Katas.FizzBuzz/FizzBuzzTests.cs
+++ Katas.FizzBuzz/FizzBuzzTests.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace Katas.FizzBuzz
 {
     [TestFixture]
     public class FizzBuzzTests
     {
+        [Test]
+        public void One_ReturnsOne()
+        {
+            Assert.That(FizzBuzz.Calculate(1), Is.EqualTo("1"));
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public static string Calculate(int n)
+        {
+            return "1";
+        } 
     }
 }
