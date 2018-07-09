68a230582563db5903e7469242c58ba1b4857636
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-09 19:48:17 +0200

    [FizzBuzz] - Initial Structure

---
+++ Katas.FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+    }
+}
