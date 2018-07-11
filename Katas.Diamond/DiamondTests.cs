68334d1b9661005b89877cef6a161dd0b418a70b
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-11 22:23:42 +0200

    [Diamond] - Diamond of A

--- Katas.Diamond/DiamondTests.cs
+++ Katas.Diamond/DiamondTests.cs
@@ -5,5 +5,18 @@ namespace Katas.Diamond
     [TestFixture]
     public class DiamondTests
     {
+        [Test]
+        public void DiamondA()
+        {
+            Assert.That(Diamond.GenerateFor('C'), Is.EqualTo("A"));
+        }
+    }
+
+    public class Diamond
+    {
+        public static string GenerateFor(char letter)
+        {
+            return "A";
+        }
     }
 }
